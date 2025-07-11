3d97cc1797ba75a75e53b44d3f5d89b4b39ee60c
Kevin Skrei <kevin@arccosgolf.com>
2025-07-10 19:00:17 -0500

    attempting to change package name

--- Package.swift
+++ Package.swift
@@ -2,14 +2,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "iOS-BLE-Library",
+    name: "IOSBLELibrary",
     platforms: [
         .iOS(.v13),
         .macOS(.v10_15),
         .watchOS(.v6)
     ],
     products: [
-        .library(name: "iOS-BLE-Library", targets: ["iOS-BLE-Library"]),
+        .library(name: "IOSBLELibrary", targets: ["IOSBLELibrary"]),
         .library(name: "iOS-BLE-Library-Mock", targets: ["iOS-BLE-Library-Mock"]),
     ],
     dependencies: [
@@ -18,8 +18,8 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
-        .target(name: "iOS-BLE-Library"),
-        .target(name: "iOS-BLE-Library-Mock", dependencies: ["iOS-BLE-Library", .product(name: "CoreBluetoothMock", package: "IOS-CoreBluetooth-Mock")]),
+        .target(name: "IOSBLELibrary"),
+        .target(name: "iOS-BLE-Library-Mock", dependencies: ["IOSBLELibrary", .product(name: "CoreBluetoothMock", package: "IOS-CoreBluetooth-Mock")]),
         .testTarget(name: "iOS-BLE-LibraryTests", dependencies: ["iOS-BLE-Library-Mock", .product(name: "CoreBluetoothMock-Collection", package: "CoreBluetoothMock-Collection")]),
     ]
 )
